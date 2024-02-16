8092895013c3686be732c3850362ad0ddf995389
maurice wainaina <mauricewainaina19@gmail.com>
2024-02-16 22:01:30 +0300

    changes on both files

--- file2.py
+++ file2.py
@@ -1,7 +1,7 @@
-import file2 
+import file1 
 
 def test_hello_world():
-    expected_output = hello-world.output
+    expected_output = "hello world"
     actual_output = hello-world.output # You can replace this with any other output to intentionally fail
     
     assert actual_output == expected_output
