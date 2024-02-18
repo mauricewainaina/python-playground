eab10f7c6bd7bbdbb14551f29f869af348cbdac1
maurice wainaina <mauricewainaina19@gmail.com>
2024-02-18 23:01:09 +0300

    added a comment

--- testing.py
+++ testing.py
@@ -9,7 +9,7 @@ import helloworld
 
 def test_hello_world():
     expected_output = "hello world"
-    actual_output = helloworld.my_variable  # You can replace this with any other output to intentionally fail
+    actual_output = helloworld.my_variable  # this line checks my variable value from helloworld file
     
     assert actual_output == expected_output
 
