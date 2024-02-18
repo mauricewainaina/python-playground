363a753301c4c2e4c1e0dff0014915b7628119de
maurice wainaina <mauricewainaina19@gmail.com>
2024-02-18 22:43:13 +0300

    adding a function in the test code

--- testing.py
+++ testing.py
@@ -1,11 +1,17 @@
 import helloworld
 
-if helloworld.my_variable == "hello world":
-    result = True
-else:
-    result = False
+# if helloworld.my_variable == "hello world":
+#     result = True
+# else:
+#     result = False
 
-print(result)
+# print(result)
+
+def test_hello_world():
+    expected_output = "hello world"
+    actual_output = helloworld.my_variable  # You can replace this with any other output to intentionally fail
+    
+    assert actual_output == expected_output
 
 
       
