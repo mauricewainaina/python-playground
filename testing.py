87c0473de0c876272236b19823cdc82d36fbee5e
maurice wainaina <mauricewainaina19@gmail.com>
2024-02-21 16:27:02 +0300

    added a comment on the assert construct

--- testing.py
+++ testing.py
@@ -11,7 +11,7 @@ def test_hello_world():
     expected_output = "hello world"
     actual_output = helloworld.my_variable  # this line checks my variable value from helloworld file
     
-    assert actual_output == expected_output
+    assert actual_output == expected_output  #this assert construct allows you to test asumptions about the code.
 
 
       
